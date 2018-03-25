086e1fd35858323db9989c929f57d53c272dc97e
Frank Klomfas <autoclone@web.de>
2018-03-25 17:52:03 +0200

    register player name

--- script/memo.js
+++ script/memo.js
@@ -12,6 +12,7 @@ $(window).ready(function(){
        */
       constructor(name)
       {
+        alert(true)
         this.name = '';
 
         this.createHtmlTag();
@@ -34,15 +35,17 @@ $(window).ready(function(){
         $('.background').show();
         if (!this.name)
         {
-          let html = '<label for="username">Name:</label><input name="username" id="username" type="text" pattern="[a-zA-Z0-9]" placeholder="Bitte nur Buchstaben und Zahlen eintragen" autofocus />';
+          let html = '<label for="username">Name:</label><input name="username" id="username" type="text" pattern="[a-zA-Z0-9]" placeholder="Bitte nur Buchstaben und Zahlen eintragen" autofocus autocomplete="on"/><input type="button" value="senden" id="submit">';
           $('#overlay').append(html);
         }
 
-        setName()
-        {
-          console.log(event.target)
-        }
+      }
 
+      setname()
+      {
+        this.name = $(event.target ).prev().val();
+        $('.gamer').text( this.name + ' ist am Zug!' );
+        $('.background, #overlay').hide();
       }
 
 
@@ -53,23 +56,16 @@ $(window).ready(function(){
     $('#go').on('click', function(){
       memo = new Memo();
       memo.start;
+      $('#submit').on('click', function(event){
+        // console.log(memo)
+        memo.setname();
+      });
     });
-
-    $('#overlay input').on('change', function(event){
-      memo.setName(event.target);
-    });
-
-
-
 
 
 
 
   })();
-
-
-
-
 
 
 
