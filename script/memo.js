3bdfcdf740496ed66ce65edfe8b0d2c25e0ca7bd
Frank Klomfas <frank@Franks-MacBook-Pro.local>
2018-03-31 18:01:19 +0200

    click event on all images

--- script/memo.js
+++ script/memo.js
@@ -14,16 +14,20 @@ $(window).ready(function(){
       {
         /**
         * @param string   name            [the name of the player]
-        * @param string   countMemory     [number of memory card]
+        * @param string   countMemo       [number of memo card]
         * @param array    numberArray     [number of cards in an array]
         * @param array    randomArray     [random number of cards in an array]
-        * @param integer  showMemoryField [number of cards that are visible]
+        * @param integer  showMemoField   [number of cards that are visible]
+        * @param string   currentImage    [active image by click]
+        * @param string   currentTarget   [active target from image by click]
         */
         this.name             = '';
-        this.countMemory      = $('#count').val();
+        this.countMemo        = $('#count').val();
         this.numberArray      = [];
         this.randomArray      = [];
-        this.showMemoryField  = []
+        this.showMemoField    = 0;
+        this.currentImage     = '';
+        this.currentTarget    = '';
 
         /**
          * start the methoden
@@ -38,14 +42,14 @@ $(window).ready(function(){
           //this.newSGametart();
         } else
         {
-          this.startCreateMemory();
+          this.startCreateMemo();
         }
         $('#go').addClass('active');
 
       }
 
       /**
-       * [start the setting for a new memory game]
+       * [start the setting for a new memo game]
        * [register the range event]
        * @return {[type]} [description]
        */
@@ -65,7 +69,7 @@ $(window).ready(function(){
             this.settingGame();
             this.setNumberArray();
             this.setRandom();
-            this.createMemoryField();
+            this.createMemoField();
           }.bind(this));
         }
       }
@@ -76,7 +80,7 @@ $(window).ready(function(){
       settingGame()
       {
         this.name = $('#username').val() != '' ? $('#username').val() : 'Anonums';
-        this.countMemory = $('#count').val();
+        this.countMemo = $('#count').val();
         $('.setting-menu').addClass('hidden');
         $('.background').hide();
         this.readHeader()
@@ -95,7 +99,7 @@ $(window).ready(function(){
       */
       readHeader()
       {
-        $('h1').after('<p> - es spielt <b>' + this.name + '</b> mit <b>' + this.countMemory + '</b> Karten.</p>')
+        $('h1').after('<p> - es spielt <b>' + this.name + '</b> mit <b>' + this.countMemo + '</b> Karten.</p>')
       }
       /**
        * [newGameStart started with a restart of the game]
@@ -106,24 +110,31 @@ $(window).ready(function(){
         console.log('new')
       }
       /**
-       * [startCreateMemory starts at the first start]
+       * [startCreateMemo starts at the first start]
        * @return {[type]} [description]
        */
-      startCreateMemory()
+      startCreateMemo()
       {
         console.log('start')
 
       }
 
+      /**
+       * [setNumberArray create a numberArray whose length is the number of cards]
+       * @return {[array]} [numberArray]
+       */
       setNumberArray()
       {
-        for (var i = 0; i < this.countMemory ; i++)
+        for (var i = 0; i < this.countMemo ; i++)
         {
           this.numberArray.push(i);
         }
         this.numberArray = this.numberArray.concat(this.numberArray);
       }
-
+      /**
+       * [setRandom create a Random array from the number number array]
+       * @return {[array]} [randomArray]
+       */
       setRandom()
       {
         let arrayLength = this.numberArray.length;
@@ -139,30 +150,83 @@ $(window).ready(function(){
 
         this.randomArray = arrguments;
       }
-
-      createMemoryField()
+      /**
+       * [createMemoField]
+       * [froms the memory cards and overwrites the placeholders ]
+       * [they are marked width "__"]
+       * [append the cards into the DOM and register the click event]
+       * @return {[string]} [showMemoField number of the visited cards]
+       */
+      createMemoField()
       {
-        let html = '<div class="item"><figure class="memory-item"><img src="images/memo/pattern.jpg" alt="" class="cards" data-no="__no__" data-random="__random__"/></figure></div>';
+        let html = '<div class="item"><figure class="memo-item"><img src="images/memo/pattern.jpg" alt="" class="cards" data-no="__no__" data-random="__random__"/></figure></div>';
         let htmlOutput;
 
         $('.game-area').hide()
-        for (var i = 0; i < (this.countMemory ) * 2; i++)
+        for (var i = 0; i < (this.countMemo ) * 2; i++)
         {
           htmlOutput ? htmlOutput += html.replace('__no__', i).replace('__random__', this.randomArray[i] ) : htmlOutput = html.replace('__no__', i).replace('__random__', this.randomArray[i] );
         }
-console.log(htmlOutput)
+
         $('.game-area')
           .append(htmlOutput)
           .show('slow');
 
+        $('.cards').on('click', function( event ){
+
+          var $target = $(event.target);
+
+          this.currentImage     = 'images/memo/img_' + $target.attr('data-random') + '.jpg';
+          this.currentTarget    = $target;
+          this.rotate();
+        }.bind(this));
+
         // set the image of the canvas
-        this.showMemoryField = this.randomArray.length;
+        this.showMemoField = this.randomArray.length;
+      }
+
+      rotate()
+      {
+
+        if ( !this.currentTarget.hasClass('rotate380') )
+        {
+          this.chanceImage();
+
+          // register the click
+          // if (Memory.init.$firstClick  == '') {
+          //   Memory.init.$firstClick = $(target);
+          // }
+          // else {
+          //
+          //   Memory.init.$currentClick = $(target);
+          // }
+          //
+          // if ( ( Memory.init.$firstClick != '' ) && ( Memory.init.$currentClick != '' ) )
+          // {
+          //   Memory.check();
+          // }
+
+        }
+        else {
+
+          //Memory.startCreateMemory( 'images/pattern.jpg', $(target) );
+        }
+
+      }
+
+      chanceImage()
+      {
+        setTimeout(function(){
+          this.currentTarget.attr('src', this.currentImage);
+        }.bind(this), 300);
+        this.currentTarget.toggleClass('rotate380');
       }
 
       clearGame()
       {
-        this.numberArray = [];
-        this.randomArray = [];
+        this.numberArray    = [];
+        this.randomArray    = [];
+        this.showMemoField  = 0;
         console.log('clear');
       }
 
@@ -175,6 +239,7 @@ console.log(htmlOutput)
     $('#go').on('click', function(){
       memo = new Memo();
       memo.start;
+      console.log(memo)
     });
 
 
